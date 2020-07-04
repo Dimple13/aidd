f151b6902c772cc1d20009c766e9d3b71f714c17
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2020-07-04 11:37:37 +0530

    Commit

--- app/Http/Controllers/SnakeController.php
+++ app/Http/Controllers/SnakeController.php
@@ -22,17 +22,18 @@ class SnakeController extends Controller
 public static function model(Request $request)
 {
    
-    $diff=$request->input('diff');
-    //return $diff;
+    // $diff=$request->input('diff');
+    // //return $diff;
    
-    $percentage=[[1, 0.0], [1, 0.1], [1,0.2], [1, 0.3], [1,0.4], [1,0.5], [1, 0.6], [1,0.7], [1,0.8],[1,0.9],[1,1.0]];
-    $score1=['0','10','20','30','40','50','60','70','80','90','100'];
+    // $percentage=[[1, 0.0], [1, 0.1], [1,0.2], [1, 0.3], [1,0.4], [1,0.5], [1, 0.6], [1,0.7], [1,0.8],[1,0.9],[1,1.0]];
+    // $score1=['0','10','20','30','40','50','60','70','80','90','100'];
 
 
-    $classifier = new SVC(Kernel::RBF);
-     $classifier->train($percentage, $score1);
+    // $classifier = new SVC(Kernel::RBF);
+    //  $classifier->train($percentage, $score1);
         
-      $pred=$classifier->predict([1,0.8]);
+    //   $pred=$classifier->predict([1,0.8]);
+    $pred=80;
     // echo($pred);
      return view('snakescore')->with('pred',$pred);
 }
--- database/migrations/2020_06_29_123256_create_admins_table.php
+++ database/migrations/2020_06_29_123256_create_admins_table.php
@@ -15,6 +15,7 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -22,7 +23,7 @@ class CreateAdminsTable extends Migration
             $table->timestamps();       
         });
  	    DB::table('admins')->insert([
-            ['email' => 'Dimple@gmail.com', 'password' => '$2y$10$jjx1nioaJF.xGxmULwSt3uKQW6sZEToUhp9hJ8.thqwowncTYDhW6'],
+            ['name'=>'Dimple','email' => 'Dimple@gmail.com', 'password' => '$2y$10$jjx1nioaJF.xGxmULwSt3uKQW6sZEToUhp9hJ8.thqwowncTYDhW6'],
         ]);
     }
 
